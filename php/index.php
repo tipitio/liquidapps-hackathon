3a6b24cf036781c5a3a71846d1c4f1f3cec5ec29
Ben Althauser <bena@h5sw.com>
2019-11-23 22:50:20 -0800

    demo home/navigation

---
+++ php/index.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1">
+
+  <!-- project remote resources -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Comfortaa&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
+
+  <!-- project local resources -->
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<!-- arrow back to main index -->
+<div class="main-wrapper">
+<div class="inner-container">
+<div class="logo">
+  <img src="img/tipit.io.svg">
+  <h1>tipit team</h1>
+</div>
+
+<!-- demo links -->
+<a href="pos-demo.php"  class="presentation-row">
+   POS Demo
+   <img src="img/hanks_logo.png">
+</a>
+<a href="author-demo.php" class="presentation-row">
+  Author Demo
+  <img src="img/mark-stair.png">
+</a>
+
+
+</div>
+</div>
+
+</body>
+</html>
